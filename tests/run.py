67723489b19f338b8c9bd513ed056e6d7aaf6963
kshramt <kshramt@example.com>
2020-07-10 20:18:49 +0900

    fix:

--- tests/run.py
+++ tests/run.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 
+import sys
 import unittest
+
+if sys.version_info.major == 3 and sys.version_info.minor == 7:
+    import py37 as test
+else:  # sys.version_info.major == 3 and sys.version_info.minor == 8
+    import latest as test
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(test)
