fa927af849bc0cd60a23238a4cc2d35d020ad3e0
kshramt <kshramt@example.com>
2020-08-08 14:09:22 +0900

    Support GetAttr[TModule, TName] (inspection mode)

--- type_casting/latest/__init__.py
+++ type_casting/latest/__init__.py
@@ -1,6 +1,8 @@
+from typing import Any, Dict, Set
 import collections
 import dataclasses
 import decimal
+import inspect
 import sys
 from typing import Any, Generic, Literal, TypeVar, Union
 
@@ -11,33 +13,54 @@ _TArgs = TypeVar("_TArgs")
 _TKwargs = TypeVar("_TKwargs")
 
 
-class GetAttr(Generic[_TModule, _TName, _TArgs, _TKwargs]):
+class Error(Exception):
     pass
+
+
+class CastingError(Error):
+    pass
+
+
+class _GetAttrOf:
+    @staticmethod
+    def __getitem__(types):
+        if len(types) == 2:
+            return _GetAttrWithInspect[types]
+        elif len(types) == 4:
+            return _GetAttrWithArgsAndKwargs[types]
+        else:
+            raise CastingError(
+                f"Only GetAttr[TModule, TName] or GetAttr[TModule, TName, TArgs, TKwargs] is supported: {types}"
+            )
+
+
+class _GetAttrWithInspect(Generic[_TModule, _TName]):
+    pass
+
+
+class _GetAttrWithArgsAndKwargs(Generic[_TModule, _TName, _TArgs, _TKwargs]):
+    pass
+
+
+GetAttr = _GetAttrOf()
 
 
 def cast(cls, x, implicit_conversions=None):
     if implicit_conversions and (cls in implicit_conversions):
         return implicit_conversions[cls](x)
     elif dataclasses.is_dataclass(cls):
-        if not isinstance(x, dict):
-            raise TypeError(f"{x}: {type(x)} is not compatible with {cls}")
-        required_key_set = set(
-            f.name
-            for f in dataclasses.fields(cls)
-            if (f.default == dataclasses.MISSING)
-            and (f.default_factory == dataclasses.MISSING)
-        )
-        x_key_set = set(x)
-        fields = {f.name: f.type for f in dataclasses.fields(cls)}
-        if not (
-            required_key_set.issubset(x_key_set) and x_key_set.issubset(set(fields))
-        ):
-            raise TypeError(f"{x}: {type(x)} is not compatible with {cls}")
-        return cls(
-            **{
-                k: cast(fields[k], v, implicit_conversions=implicit_conversions)
-                for k, v in x.items()
-            }
+        fields = dataclasses.fields(cls)
+        return _cast_kwargs(
+            cls,
+            x,
+            implicit_conversions,
+            {f.name: f.type for f in fields},
+            set(
+                f.name
+                for f in fields
+                if (f.default == dataclasses.MISSING)
+                and (f.default_factory == dataclasses.MISSING)
+            ),
         )
     elif (
         isinstance(cls, type)
@@ -45,49 +68,67 @@ def cast(cls, x, implicit_conversions=None):
         and hasattr(cls, "__annotations__")
         and hasattr(cls, "__total__")
     ):
-        if not isinstance(x, dict):
-            raise TypeError(f"{x}: {type(x)} is not compatible with {cls}")
-        fields = cls.__annotations__
-        if cls.__total__:
-            if set(fields) != set(x):
-                raise TypeError(f"{x}: {type(x)} is not compatible with {cls}")
-        else:
-            if not set(x).issubset(set(fields)):
-                raise TypeError(f"{x}: {type(x)} is not compatible with {cls}")
-        return {
-            k: cast(fields[k], v, implicit_conversions=implicit_conversions)
-            for k, v in x.items()
-        }
+        return _cast_kwargs(
+            cls,
+            x,
+            implicit_conversions,
+            cls.__annotations__,
+            set(cls.__annotations__) if cls.__total__ else set(),
+        )
     elif cls == Any:
         return x
     elif cls == decimal.Decimal:
         if not isinstance(x, (str, int, float)):
-            raise TypeError(f"{x}: {type(x)} is not compatible with {cls}")
+            raise CastingError(f"{x}: {type(x)} is not compatible with {cls}")
         return decimal.Decimal(x)
     elif cls == complex:
         if not isinstance(x, (int, float, complex)):
-            raise TypeError(f"{x}: {type(x)} is not compatible with {cls}")
+            raise CastingError(f"{x}: {type(x)} is not compatible with {cls}")
         return x
     elif cls == float:
         if not isinstance(x, (int, float)):
-            raise TypeError(f"{x}: {type(x)} is not compatible with {cls}")
+            raise CastingError(f"{x}: {type(x)} is not compatible with {cls}")
         return x
     elif isinstance(cls, type):
         if not isinstance(x, cls):
-            raise TypeError(f"{x}: {type(x)} is not compatible with {cls}")
+            raise CastingError(f"{x}: {type(x)} is not compatible with {cls}")
         return x
-    elif cls.__origin__ == GetAttr:
+    elif cls.__origin__ == _GetAttrWithArgsAndKwargs:
         if "module" not in x:
-            raise TypeError(f'The "module" key not found in `x` for {cls}: {x}')
+            raise CastingError(f'The "module" key not found in `x` for {cls}: {x}')
         if "name" not in x:
-            raise TypeError(f'The "name" key not found in `x` for {cls}: {x}')
+            raise CastingError(f'The "name" key not found in `x` for {cls}: {x}')
         module, name, args, kwargs = cls.__args__
         return getattr(sys.modules[cast(module, x["module"])], cast(name, x["name"]))(
             *cast(args, x.get("args", [])), **cast(kwargs, x.get("kwargs", {}))
         )
+    elif cls.__origin__ == _GetAttrWithInspect:
+        if "module" not in x:
+            raise CastingError(f'The "module" key not found in `x` for {cls}: {x}')
+        if "name" not in x:
+            raise CastingError(f'The "name" key not found in `x` for {cls}: {x}')
+        module, name = cls.__args__
+        fn = getattr(sys.modules[cast(module, x["module"])], cast(name, x["name"]))
+        fields = {}
+        required_key_set = set()
+        for p in inspect.signature(fn).parameters.values():
+            if p.annotation == inspect.Signature.empty:
+                parameters = tuple(
+                    dict(name=p.name, annotation=p.annotation, default=p.default)
+                    for p in inspect.signature(fn).parameters.values()
+                )
+                raise ValueError(
+                    f"Unable to get the type annotation of {p.name} for {fn}{parameters}. Please use `GetAttr[module, name, args_type, kwargs_type]` instead."
+                )
+            fields[p.name] = p.annotation
+            if p.default == inspect.Signature.empty:
+                required_key_set.add(p.name)
+        return _cast_kwargs(
+            fn, x.get("kwargs", {}), implicit_conversions, fields, required_key_set
+        )
     elif cls.__origin__ == Literal:
         if x not in cls.__args__:
-            raise TypeError(f"{x} is not compatible with {cls}")
+            raise CastingError(f"{x} is not compatible with {cls}")
         return x
     elif cls.__origin__ in (set, collections.abc.Set, collections.abc.MutableSet,):
         vcls = cls.__args__[0]
@@ -119,7 +160,7 @@ def cast(cls, x, implicit_conversions=None):
     elif cls.__origin__ == tuple:
         vclss = cls.__args__
         if len(vclss) != len(x):
-            raise TypeError(f"{x}: {type(x)} is not compatible with {cls}")
+            raise CastingError(f"{x}: {type(x)} is not compatible with {cls}")
         return tuple(
             cast(vcls, v, implicit_conversions=implicit_conversions)
             for vcls, v in zip(vclss, x)
@@ -128,8 +169,24 @@ def cast(cls, x, implicit_conversions=None):
         for ucls in cls.__args__:
             try:
                 return cast(ucls, x, implicit_conversions=implicit_conversions)
-            except TypeError:
+            except CastingError:
                 pass
-        raise TypeError(f"{x}: {type(x)} is not compatible with {cls}")
+        raise CastingError(f"{x}: {type(x)} is not compatible with {cls}")
     else:
         raise ValueError(f"Unsupported class {cls}: {type(cls)}")
+
+
+def _cast_kwargs(
+    cls, x, implicit_conversions, fields: Dict[str, Any], required_key_set: Set[str]
+):
+    if not isinstance(x, dict):
+        raise CastingError(f"{x}: {type(x)} is not compatible with {cls}")
+    x_key_set = set(x)
+    if not (required_key_set.issubset(x_key_set) and x_key_set.issubset(set(fields))):
+        raise CastingError(f"{x}: {type(x)} is not compatible with {cls}")
+    return cls(
+        **{
+            k: cast(fields[k], v, implicit_conversions=implicit_conversions)
+            for k, v in x.items()
+        }
+    )
